22ee090cc14955f9536453a6c2366891d93bad81
Abdellah <abdellah.ait59@gmail.com>
2025-05-21 17:17:53 +0200

    feat(shopping-list.js): Enhance addItem function with validation and edit/delete button functionality

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -8,4 +8,32 @@ function addItem(){
     let itemInput = document.getElementById('itemInput');
     let itemText= itemInput.value;
 
+    if(itemText === ""){
+
+        return;
+    }
+
+    let li = document.createElement('li');
+    li.innerHTML = itemText;
+
+    let editButton= document.createAttribute('button');
+
+    editButton.innerHTML= '<ion-icon name="pencil-outline"></ion-icon>';
+
+    editButtononclicl = function(){
+        editItem(li);
+    }
+
+
+    let deleteButton= document.createElement('button');
+
+    deleteButton.innerHTML= '<ion-icon name="trash-outline"></ion-icon>';
+
+    deleteButton.oneclick= function(){
+
+        deleteItem(li);
+    }
+
+
+
 }
