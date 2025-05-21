ff791faac7caedcd51e916e04dbd9571b775530a
Abdellah <abdellah.ait59@gmail.com>
2025-05-21 17:07:28 +0200

    feat(shopping-list.js): Create shopping list functionality with initial addItem function

---
+++ js/shopping-list.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let shoppingList = document.getElementById('shoppingList');
+
+
+function addItem(){
+
+    let itemInput = document.getElementById('itemInput');
+    let itemText= itemInput.value;
+
+}
